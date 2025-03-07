32ebabab90a1560f96176aa73c42ed02e2f6c094
leewootak <leewt32414@gmail.com>
2025-03-07 13:01:24 +0900

    [feat] 아이템 데이터 추가

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public PlayerController controller;
     public PlayerCondition condition;
+
+    public ItemData itemData;
+    public Action addItem;
 
     private void Awake()
     {
